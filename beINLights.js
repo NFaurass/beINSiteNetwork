10a32d7f2603294170d4b80b7a1a3d93cff87b82
NFaurass <36932043+NFaurass@users.noreply.github.com>
2018-07-15 13:50:22 +0000

    Create beINLights.js

---
+++ beINLights.js
@@ -0,0 +1,13 @@
+function LightsOff() {
+    document.getElementById("BGS").style.background = "#28313b";
+    document.getElementById("AMC").style.background = "#28313b";
+    document.getElementById("Foot").style.background = "#28313b";
+    document.getElementById("beINSH").style.background = "#28313b";
+}
+function LightsOn() {
+    document.getElementById("BGS").style.background = "url(https://thumbs.dreamstime.com/b/russian-world-cup-soccer-football-championship-white-russia-silhouette-icon-logo-seamless-background-abstract-pattern-vector-105685861.jpg)";
+    document.getElementById("AMC").style.background = "url(https://png.pngtree.com/thumb_back/fw800/back_pic/03/78/97/9957c247fda25ff.jpg) no-repeat";
+    document.getElementById("AMC").style.backgroundSize = "cover";
+    document.getElementById("beINSH").style.background = "#28313b";
+    document.getElementById("Foot").style.background = "#28313b";
+}
