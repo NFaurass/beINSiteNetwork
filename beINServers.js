6c31808ef0f827b93f9e44b2c30f1c1645df1c73
NFaurass <36932043+NFaurass@users.noreply.github.com>
2018-07-15 13:51:22 +0000

    Create beINServers.js

---
+++ beINServers.js
@@ -0,0 +1,15 @@
+function beINMax1HD480p() {
+    document.getElementById("LiveStream").src = "http://widestream.io/embed-37284";
+}
+function beINMax1HD360p() {
+    document.getElementById("LiveStream").src = "http://widestream.io/embed-37285";
+}
+function beINMax2HD480p() {
+    document.getElementById("LiveStream").src = "http://widestream.io/embed-37287";
+}
+function beINMax2HD360p() {
+    document.getElementById("LiveStream").src = "http://widestream.io/embed-37286";
+}
+function BBCOneHD480p() {
+    document.getElementById("LiveStream").src = "http://widestream.io/embed-37289";
+}
